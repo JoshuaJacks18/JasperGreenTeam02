3eb5baf8cfc1c37c30eee7e04a03db8d57f48cd1
Josh <joshuajacks18@gmail.com>
2022-11-16 10:45:44 -0600

    Added Crew Controller and Views.

--- JasperGreenTeam02/Models/JasperGreenContext.cs
+++ JasperGreenTeam02/Models/JasperGreenContext.cs
@@ -40,7 +40,7 @@ namespace JasperGreenTeam02.Models
             modelBuilder.Entity<Crew>()
                 .HasOne(e => e.Foreman)
                 .WithMany(c => c.Crews)
-                .OnDelete(DeleteBehavior.Restrict)
+                .OnDelete(DeleteBehavior.SetNull)
             ;
 
             modelBuilder.Entity<Crew>()
@@ -432,35 +432,35 @@ namespace JasperGreenTeam02.Models
                 new Crew
                 { 
                     CrewID = 1,
-                    CrewForemanID = 1,
+                    ForemanID = 1,
                     CrewMember1ID = 2,
                     CrewMember2ID = 3
                 }, 
                 new Crew
                 {
                     CrewID = 2,
-                    CrewForemanID = 4,
+                    ForemanID = 4,
                     CrewMember1ID = 5,
                     CrewMember2ID = 6
                 },
                 new Crew
                 {
                     CrewID = 3,
-                    CrewForemanID = 7,
+                    ForemanID = 7,
                     CrewMember1ID = 8,
                     CrewMember2ID = 9
                 },
                 new Crew
                 {
                     CrewID = 4,
-                    CrewForemanID = 10,
+                    ForemanID = 10,
                     CrewMember1ID = 11,
                     CrewMember2ID = 12
                 },
                 new Crew
                 {
                     CrewID = 5,
-                    CrewForemanID = 13,
+                    ForemanID = 13,
                     CrewMember1ID = 14,
                     CrewMember2ID = 15
                 }
