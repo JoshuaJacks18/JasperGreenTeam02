606c208486d73e790e14d58e6fba8cc637947f12
jsjiang_tamu2023 <jsjiang_tamu2023@LABS-153-03>
2022-12-05 12:15:04 -0600

    documentation for the view models

--- JasperGreenTeam02/ViewModels/CrewViewModel.cs
+++ JasperGreenTeam02/ViewModels/CrewViewModel.cs
@@ -1,4 +1,17 @@
-﻿using JasperGreenTeam02.Models;
+﻿//AUTHOR:   Joshua Jacks, Justin Jiang 
+//COURSE:   ISTM 415 Section 501
+//FORM:  CrewViewModel.cs 
+//PURPOSE:  This view model passes data between the CrewController
+//and the Crew Add/Edit view.
+//INITIALIZE: This class is initiated when the add/edit crew view is opened.
+//INPUT:  References from opening of a crew view.
+//PROCESS:  Data is referenced in the add/edit crew view and the data is passed
+//to the view by the view model.
+//OUTPUT:   Data passed to the add/edit crew view.
+//HONOR CODE: “On my honor, as an Aggie, I have neither given  
+//   nor received unauthorized aid on this academic  
+//   work.” 
+using JasperGreenTeam02.Models;
 using System.Collections.Generic;
 
 namespace JasperGreenTeam02.ViewModels
